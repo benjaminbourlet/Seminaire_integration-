a45a4ce69eccfe37900336daad684a375f2e7ed3
Benjamin FOUILHAC <bfouilhac@gmail.com>
2025-09-17 16:16:39 +0200

    ajout BIP quand connexion WIFI OK bug sur les requêtes API (changer entre HTTPS et HTTP)

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import network
 import urequests
 import time
 import neopixel
-from machine import Pin
+from machine import Pin, PWM
 
-PIN_DATA = 18 
-nb_leds = 2         
+PIN_DATA = 18
+nb_leds = 2
 led = neopixel.NeoPixel(Pin(PIN_DATA, Pin.OUT), nb_leds)
 
 
 def getweather():
     ville = "Bordeaux"
     res = {}
-    data = urequests.get("https://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=c1c60bb1bc8fbdcb97ee83119e4cc2c6&units=metric").json()
+    rawdataget = urequests.get(
+        "http://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=c1c60bb1bc8fbdcb97ee83119e4cc2c6&units=metric",
+        timeout=5)
+    data = rawdataget.json()
 
     if "message" in data and "cod" in data:
         print("Erreur de l'API : " + data["message"] + ", Code : " + data["cod"])
@@ -32,6 +35,7 @@ def getweather():
     res["pressure"] = data["main"]["pressure"]
     return res
 
+
 def setupWifiCon():
     try:
         wifiCon = network.WLAN(network.STA_IF)
@@ -42,66 +46,80 @@ def setupWifiCon():
     except OSError as e:
         print("Erreur de connexion wifi : " + str(e))
         return False
-            
-#allumage des leds 
-def set_led(status,color):
+
+
+# allumage des leds
+def set_led(status, color):
     if status == 0:
-        led[0] = (0,0,0); led.write()
-        led[1] = (0,0,0); led.write()
+        led[0] = (0, 0, 0);
+        led.write()
+        led[1] = (0, 0, 0);
+        led.write()
     elif status == 1:
         if color == 'green':
-            led[0] = (0, 255, 0); led.write()
+            led[0] = (0, 255, 0);
+            led.write()
         elif color == 'red':
-            led[0] = (255, 0, 0); led.write()
+            led[0] = (255, 0, 0);
+            led.write()
         elif color == 'orange':
-            led[0] = (255, 150, 0); led.write()
+            led[0] = (255, 150, 0);
+            led.write()
     elif status == 3:
         if color == 'green':
-            led[0] = (0, 255, 0); led.write()
-            led[1] = (0, 255, 0); led.write()
+            led[0] = (0, 255, 0);
+            led.write()
+            led[1] = (0, 255, 0);
+            led.write()
         elif color == 'red':
-            led[0] = (255, 0, 0); led.write()
-            led[1] = (255, 0, 0); led.write()
+            led[0] = (255, 0, 0);
+            led.write()
+            led[1] = (255, 0, 0);
+            led.write()
         elif color == 'orange':
-            led[0] = (255, 150, 0); led.write()
-            led[1] = (255, 150, 0); led.write()
+            led[0] = (255, 150, 0);
+            led.write()
+            led[1] = (255, 150, 0);
+            led.write()
         else:
             status = 0
-            
+
+
 # logique d'allumage des leds
 def setledwind(strength_wind):
     if strength_wind == 0:
         # 1 vert
-        set_led(0,'')
+        set_led(0, '')
     elif 1 <= strength_wind <= 2:
         # 2 verts
-        set_led(3,'green')
+        set_led(3, 'green')
     elif 3 <= strength_wind <= 4:
         # 1 orange
-        set_led(1,'orange')
+        set_led(1, 'orange')
     elif 5 <= strength_wind <= 6:
         # 2 orange
-        set_led(3,'orange')
+        set_led(3, 'orange')
     elif 7 <= strength_wind <= 8:
         # 1 rouge
-        set_led(1,'red')
+        set_led(1, 'red')
     elif 9 <= strength_wind <= 10:
         # 2 rouge
-        set_led(3,'red')
+        set_led(3, 'red')
     elif 11 <= strength_wind <= 12:
-        for i in range (3):
-            set_led(3,'red')
+        for i in range(3):
+            set_led(3, 'red')
             time.sleep(1)
-            set_led(0,'')
+            set_led(0, '')
             time.sleep(1)
-            set_led(3,'red')
+            set_led(3, 'red')
             time.sleep(1)
-            set_led(0,'')
+            set_led(0, '')
             i += 1
-        #2 rouge clignotant
+        # 2 rouge clignotant
     else:
-        set_led(0,'')
-        
+        set_led(0, '')
+
+
 # Fonction de conversion de la vitesse du vent en coefficient de beaufort
 def windconversion(windspeed):
     if windspeed == 0:
@@ -109,19 +127,35 @@ def windconversion(windspeed):
     elif windspeed >= 32.7:
         return 12
     else:
-        return int(0.836 * (windspeed ** (2/3)) + 0.5)
+        return int(0.836 * (windspeed ** (2 / 3)) + 0.5)
+
+
+def playsoundonconnection(speaker, freq, dur):
+    if freq > 0:
+        speaker.freq(freq)
+        speaker.duty(256)
+        time.sleep(dur)
+        speaker.duty(0)
+        speaker.deinit()
+
+
+# PROGRAMME PRINCIPAL
 
 # Création et reset des LEDS rouge/vert et connexion
 greenled = Pin(0, Pin.OUT)
 redled = Pin(4, Pin.OUT)
 connexionled = Pin(27, Pin.OUT)
+speakerPin = Pin(25, Pin.OUT)
+speakerPWM = PWM(speakerPin)
 
 greenled.off()
 redled.off()
 connexionled.off()
+speakerPWM.duty(0)
 
 if setupWifiCon():
     connexionled.on()
+    # playsoundonconnection(speakerPWM, 5000, 0.25)
     print("Connexion Wifi OK")
     if getweather() == -1:
         while True:
@@ -129,7 +163,7 @@ if setupWifiCon():
             time.sleep(1)
             redled.off()
             time.sleep(1)
-    else: # Boucle principale autonome de l'appareil
+    else:  # Boucle principale autonome de l'appareil
         while True:
             redled.off()
             greenled.off()

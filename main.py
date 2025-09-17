149ed81282b984aad3e7ca36150d43e073f9fb28
Benjamin FOUILHAC <bfouilhac@gmail.com>
2025-09-17 14:25:57 +0200

    gestion erreur connexion (led connexion clignote) refacto beaufort

--- main.py
+++ main.py
@@ -33,54 +33,46 @@ def getweather():
     return res
 
 def setupWifiCon():
-    wifiCon = network.WLAN(network.STA_IF)
-    wifiCon.active(True)
-    wifiCon.scan()
     try:
-        wifiCon.connect('Iphone de benji', 'a1z2e3r4t5')
+        wifiCon = network.WLAN(network.STA_IF)
+        wifiCon.active(True)
+        wifiCon.scan()
+        wifiCon.connect('POCO X7 Pro Benjamin', 'benjamin1234')
         return True
-    except:
-        print("Connexion Wifi NOK")
-        while True: # Sinon on toggle
-            connexionled.toggle()
-
-#////////////////////////////////////////////////////////////////////////#
-            #//indicateur leds force du vent//#
+    except OSError as e:
+        print("Erreur de connexion wifi : " + str(e))
+        return False
             
 #allumage des leds 
 def set_led(status,color):
-    
     if status == 0:
         led[0] = (0,0,0); led.write()
         led[1] = (0,0,0); led.write()
     elif status == 1:
         if color == 'green':
-            led[0] = (0, 255, 0); led.write();
+            led[0] = (0, 255, 0); led.write()
         elif color == 'red':
-            led[0] = (255, 0, 0); led.write();
+            led[0] = (255, 0, 0); led.write()
         elif color == 'orange':
-            led[0] = (255, 150, 0); led.write();
-        else:
-            status = 0
+            led[0] = (255, 150, 0); led.write()
     elif status == 3:
         if color == 'green':
-            led[0] = (0, 255, 0); led.write();
-            led[1] = (0, 255, 0); led.write();
+            led[0] = (0, 255, 0); led.write()
+            led[1] = (0, 255, 0); led.write()
         elif color == 'red':
-            led[0] = (255, 0, 0); led.write();
-            led[1] = (255, 0, 0); led.write();
+            led[0] = (255, 0, 0); led.write()
+            led[1] = (255, 0, 0); led.write()
         elif color == 'orange':
-            led[0] = (255, 150, 0); led.write();
-            led[1] = (255, 150, 0); led.write();
+            led[0] = (255, 150, 0); led.write()
+            led[1] = (255, 150, 0); led.write()
         else:
             status = 0
             
-#logique d'allumage des leds 
-def led_wind(strength_wind):
-    
+# logique d'allumage des leds
+def setledwind(strength_wind):
     if strength_wind == 0:
         # 1 vert
-        set_led(1,'green')
+        set_led(0,'')
     elif 1 <= strength_wind <= 2:
         # 2 verts
         set_led(3,'green')
@@ -106,50 +98,18 @@ def led_wind(strength_wind):
             time.sleep(1)
             set_led(0,'')
             i += 1
-        #2 rouge clignotent 
+        #2 rouge clignotant
     else:
         set_led(0,'')
         
-#conversion m/s en force que l'echelle de beaufort 
-def wind_conversion(wind_speed):
-    strength_wind = None
-    
-    if wind_speed <= 0.2:
-        strength_wind = 0
-    elif 0.2 < wind_speed <= 1.5:
-        strength_wind = 1
-    elif 1.5 < wind_speed <= 3.3:
-        strength_wind = 2
-    elif 3.3 < wind_speed <= 5.4:
-        strength_wind = 3
-    elif 5.4 < wind_speed <= 7.9:
-        strength_wind = 4
-    elif 7.9 < wind_speed <= 10.7:
-        strength_wind = 5
-    elif 10.7 < wind_speed <= 13.8:
-        strength_wind = 6
-    elif 13.8 < wind_speed <= 17.1:
-        strength_wind = 7
-    elif 17.1 < wind_speed <= 20.7:
-        strength_wind = 8
-    elif 20.7 < wind_speed <= 24.4:
-        strength_wind = 9
-    elif 24.4 < wind_speed <= 28.4:
-        strength_wind = 10
-    elif 28.4 < wind_speed <= 32.6:
-        strength_wind = 11
-    else:  # >= 32.7
-        strength_wind = 12
-
-    return strength_wind
-
-
-            #//fin indicateur leds force du vent//#
-#////////////////////////////////////////////////////////////////////////#
-
-
-
-
+# Fonction de conversion de la vitesse du vent en coefficient de beaufort
+def windconversion(windspeed):
+    if windspeed == 0:
+        return 0
+    elif windspeed >= 32.7:
+        return 12
+    else:
+        return int(0.836 * (windspeed ** (2/3)) + 0.5)
 
 # Création et reset des LEDS rouge/vert et connexion
 greenled = Pin(0, Pin.OUT)
@@ -168,21 +128,19 @@ if setupWifiCon():
             redled.on()
             time.sleep(1)
             redled.off()
+            time.sleep(1)
     else: # Boucle principale autonome de l'appareil
         while True:
             redled.off()
             greenled.off()
-            
-            #bloc indicateur force du vent
-            led_wind(wind_conversion(getweather()["wind"]))
-            print(wind_conversion(getweather()["wind"]))
-            
-            
-            if (getweather()["id_meteo"]) >= 800:
+            currentweather = getweather()
+            print(currentweather)
+            setledwind(windconversion(currentweather["wind"]))
+            print(windconversion(currentweather["wind"]))
+            if (currentweather["id_meteo"]) >= 800:
                 greenled.on()
                 redled.off()
             else:
                 greenled.off()
                 redled.on()
             time.sleep(15)
-            set_led(0,'')
